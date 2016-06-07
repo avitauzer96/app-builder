196904baa945308fe8b0e64bf8d2ea539fa1b39e
Avetik <avardanyan@justcoded.com>
2016-06-07 13:31:05 +0300

    Return back

--- js/controllers/lastNewsCtrl.js
+++ js/controllers/lastNewsCtrl.js
@@ -1,7 +1,7 @@
 angular.module("feedsyApp.controllers.lastNewsCtrl", [
   'feedsyApp.services.articlesService'
 ])
-  .controller('lastNewsCtrl', function ($scope, $ionicSlideBoxDelegate, $ionicHistory, $state, $log, $window, articles, height, articlesService) {
+  .controller('lastNewsCtrl', function ($scope, $ionicSlideBoxDelegate, $ionicHistory, $state, $log, $window, articles, articlesService) {
     $log.debug('init lastNewsCtrl');
     $ionicSlideBoxDelegate.update();
     if ($scope.offline) {
@@ -12,7 +12,6 @@ angular.module("feedsyApp.controllers.lastNewsCtrl", [
     }
 
     $scope.articles = articles;
-    $scope.height = height + 75;
     $scope.showMore = function () {
       return (articles.length % 18 == 0 && articles.length != 0);
     };
